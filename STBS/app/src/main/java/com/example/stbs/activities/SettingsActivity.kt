4711831c8887964dc64618fa1e20277dc5decc65
HectorGH97 <111985843+HectorGH97@users.noreply.github.com>
2022-12-02 13:22:05 -0600

    Add files via upload

---
+++ STBS/app/src/main/java/com/example/stbs/activities/SettingsActivity.kt
@@ -0,0 +1,45 @@
+package com.example.stbs.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.example.stbs.R
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_settings.*
+
+
+private const val TAG = "SettingsActivity"
+class SettingsActivity : AppCompatActivity() {
+
+    private val auth = Firebase.auth
+
+    private val current_mail = auth.currentUser?.email
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
+        Log.d(TAG, "onCreate: hello $current_mail")
+
+        welcome_user.text = "Welcome $current_mail"
+
+        change_pwd_btn.setOnClickListener {
+            auth.sendPasswordResetEmail(current_mail.toString())
+            Toast.makeText(this,"A mail to change password has been sent to $current_mail",Toast.LENGTH_LONG).show()
+        }
+
+        logout_btn.setOnClickListener {
+            auth.signOut().also {
+                startActivity(Intent(this,LoginActivity::class.java))
+            }
+        }
+
+        settings_back_btn.setOnClickListener {
+            onBackPressed()
+        }
+
+    }
+}
